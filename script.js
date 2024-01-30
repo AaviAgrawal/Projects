d1f43d3969c3a0f3148ce2c005b027cfdf3d67e7
AaviAgrawal <157015647+AaviAgrawal@users.noreply.github.com>
2024-01-31 01:38:51 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -16,18 +16,18 @@ function loadingAnimation() {
         duration: 1,
         ease: "power4.out",
     })
-    t1.from(".text h1",{
-        y:200,
-        opacity:0,
-        stagger:{
-            amount:0.5
+    t1.from(".text h1", {
+        y: 200,
+        opacity: 0,
+        stagger: {
+            amount: 0.5
         }
     })
-    t1.from("#nav",{
-        opacity:0,
-        delay:-0.5
+    t1.from("#nav", {
+        opacity: 0,
+        delay: -0.5
     })
-    
+
     var grow = 0
     var loading = document.querySelector("#loading h5")
     var int = setInterval(function () {
@@ -43,9 +43,58 @@ function loadingAnimation() {
 }
 loadingAnimation()
 
-function page1Animation() {
-    
+function page2Animation() {
+    var videoC = document.querySelector("#video-container")
+    videoC.addEventListener("mouseenter", function () {
+        gsap.to(".mousefollower", {
+            opacity: 0,
+        })
+    })
+    videoC.addEventListener("mouseleave", function () {
+        gsap.to(".mousefollower", {
+            opacity: 1,
+        })
+        gsap.to("#play-btn", {
+            left: "68%",
+            top: "-7%",
+        })
+    })
+    var videoVideo = document.querySelector("#video-container video")
+    var flag = 0
+    videoC.addEventListener("click", function () {
+        if (flag == 0) {
+            gsap.to(videoVideo, {
+                opacity: 1
+            })
+            videoVideo.play();
+            document.querySelector("#play-btn").innerHTML = `<i class="ri-pause-line"></i>`
+            gsap.to("#play-btn", {
+                scale: 0.7
+            })
+            flag = 1
+        }
+        else {
+            gsap.to(videoVideo, {
+                opacity: 0
+            })
+            videoVideo.pause();
+            document.querySelector("#play-btn").innerHTML = `<i class="ri-play-fill"></i>`
+            gsap.to("#play-btn", {
+                scale: 1
+            })
+            flag = 0
+        }
+
+    })
+    videoC.addEventListener("mousemove", function (dets) {
+        gsap.to("#play-btn", {
+            left: dets.x - 450,
+            top: dets.y - 200
+        })
+    })
 }
+page2Animation()
+
 
 Shery.mouseFollower();
 
